d589ea3e9ed2c161ef55a9d8cfcc2f46be5e066d
Atika Zeynep Evmez <91607254+atikazeynep@users.noreply.github.com>
2024-11-06 20:37:11 +0300

    Add or update file code.py

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 def greet_the_student():
-    return "22002633 \how are you?"
+    return "Ben saksı değilim!"
 if(__name__ == "__main__"):
     print(greet_the_student())
